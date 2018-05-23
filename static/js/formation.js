f2ebc26407257e4f04dcc3200534567aa4e3c146
edmond120 <edmondwong888@msn.com>
2018-05-23 14:51:47 -0400

    prepared test json and html file

--- static/js/formation.js
+++ static/js/formation.js
@@ -11,7 +11,7 @@ the json is formatted like this:
 				},
 	"songInfo":{
 					"songName"  :"<songName>",
-					"songLength":<songLength>,
+					"songLength":<song length in seconds>,
 			   },
 	"users":[
 				{
@@ -26,9 +26,7 @@ the json is formatted like this:
 	"formations":[
 				{
 					"timeTillNext" :<seconds till next frame>,
-					"userMovements":{
-										"<username>":{
-														"xcor":<xcor>,
+					"userMovements":{ "<username>":{ "xcor":<xcor>,
 														"ycor":<ycor>,
 													 }
 										...
@@ -45,3 +43,63 @@ the json is formatted like this:
 function parseFormationData(data){
 	json = JSON.parse(data);
 }
+
+//debugging begins here
+testJson = {
+				"pieceName":"testPiece",
+				"stageSize":{
+								"length":10
+								"width" :5]
+							},
+				"songInfo" :{
+								"songName"  :"abcSong",
+								"songLength":300,
+							},
+				"users"    :[
+								{
+									"username":"edmond",
+									"color"   :"red",
+								},
+								{
+									"username":"kerry",
+									"color"   :"blue",
+								},
+								{
+									"username":"fabiha",
+									"color"   :"green",
+								},
+								{
+									"username":"philip",
+									"color"   :"black",
+								},
+							],
+				"formations":[
+								{
+									"timeTillNext" :2,
+									"userMovements":{
+														"edmond":{
+																	"xcor":5,
+																	"ycor":2.5,
+																 },
+														"kerry" :{
+																	"xcor":0,
+																	"ycor":0,
+																 },
+														"fabiha":{
+																	"xcor":2,
+																	"ycor":5,
+																 },
+														"philip":{
+																	"xcor":10,
+																	"ycor":3,
+																 },
+													},
+									"userTags"     :{
+														"edmond":"tag0",
+														"kerry" :"tag1",
+														"fabiha":"tag with new line\nfor testing",
+														"philip":"extra long tag for testing",
+													},
+								},
+							],
+		   }
